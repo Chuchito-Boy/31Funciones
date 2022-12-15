3740495181601ce7a422b70487317ffdb1c70a82
luisdiego4k <gsantibamoran@gmail.com>
2022-12-14 21:54:27 -0600

    Ordenación y en el ejercicio 10 ya aparece el nombre

--- newProyecto12.py
+++ newProyecto12.py
@@ -27,8 +27,22 @@ def Movies2(tupla:tuple)->tuple:
     my_list = sorted(tupla, key = lambda x: int(x[4]), reverse=True)
     return tuple(my_list)
 
+
+# Funcion que recibe y retorna una tupla de tuplas donde se filtra las peliculas que sean del genero Action,Adventure,Drama
+def Movies3(tupla: tuple)-> tuple:
+    my_list = filter(lambda x: x[2]=="Action,Adventure,Drama", tupla)
+    return tuple(my_list)
+
+
+#Funcion que recibe una tupla de tuplas de todas las peliculas del genero 'Animation,Adventure,Comedy'
+# y ademas sean Clasificacion C, y al final retorne las tuplas ordenadas por nombre ascendente
+def Movies4(tupla: tuple)-> tuple:
+    my_list1 = filter(lambda x: x[2]=="Animation,Adventure,Comedy" and x[6]=="C" , tupla)
+    my_list = sorted(my_list1, key = lambda x: x[0])
+    return tuple(my_list)
+
 # Funcion que recibe y retorna una tupla de tuplas de todas las peliculas del genero 'Drama,Romance'
-def Movies3(genero:str):
+def Movies5(genero:str):
     def filtrar_genero(tupla:tuple)->tuple:
         my_list = [i for i in tupla if i[2]==genero]
         return tuple(my_list)
@@ -40,7 +54,7 @@ Funcion que recibe y retorna una tupla de tuplas, donde:
     Si se ingresa "Buena", se mostraran las peliculas con "Movie Rating">=6 y "Movie Rating"<9
     Si se ingresa "Mala", se mostraran las peliculas con "Movie Rating" menor a 6
 """
-def Movies4(tupla:tuple):
+def Movies6(tupla:tuple):
     list1 = [i for i in tupla if float(i[3])>=9.0]
     list2 = [i for i in tupla if float(i[3])>=6.0 and float(i[3])<9.0]
     list3 = [i for i in tupla if float(i[3])<6.0]
@@ -53,17 +67,6 @@ def Movies4(tupla:tuple):
             return tuple(list3)
     return agregar_recomendacion
 
-# Funcion que recibe y retorna una tupla de tuplas donde se filtra las peliculas que sean del genero Action,Adventure,Drama
-def Movies5(tupla: tuple)-> tuple:
-    my_list = filter(lambda x: x[2]=="Action,Adventure,Drama", tupla)
-    return tuple(my_list)
-
-#Funcion que recibe una tupla de tuplas de todas las peliculas del genero 'Animation,Adventure,Comedy'
-# y ademas sean Clasificacion C, y al final retorne las tuplas ordenadas por nombre ascendente
-def Movies6(tupla: tuple)-> tuple:
-    my_list1 = filter(lambda x: x[2]=="Animation,Adventure,Comedy" and x[6]=="C" , tupla)
-    my_list = sorted(my_list1, key = lambda x: x[0])
-    return tuple(my_list)
 
 # Funcion que recibe y retorna una tupla de tuplas de todas las peliculas donde el rating sea mayor o igual\
 # al que se le manda y ademas el año sea mayor o igual al mandado
@@ -96,7 +99,7 @@ def Movies8(tupla: tuple):
 
 # Funcion que recibe y retorna una tupla de tuplas donde se agregue un campo extra a cada tupla dependiendo
 # de la calificacion 'Movie Rating'.
-def Movies9(): 
+def Movies9():
     def agregar_recomendacion2(tupla:tuple)->tuple:
         list_aux = list(tupla)
         if float(tupla[3])>=9.0:
@@ -111,29 +114,30 @@ def Movies9():
 #Funcion que recibe y retorna una tupla de tuplas que contenga 'Movie Rating' expresado en
 # %, es decir, 9.0 ->90%, 6.2 ->62%, etc) de todas las peliculas de clasificacion 'C'
 def Movies10(tupla:tuple):
-    list_aux = [(i[3]) for i in tupla if i[6]=="C"]
+    list_aux = [(i[0],i[3]) for i in tupla if i[6]=="C"]
     def porcentual()->tuple:
-        my_list = map(lambda x:str(int(float(x[0])*10))+'%',list_aux)
-        return tuple(my_list)    
+        my_list = map(lambda x: x[0]+','+str(int(float(x[1])*10))+'%' ,list_aux)
+        return tuple(my_list)
     return porcentual
 
 if __name__ == '__main__':
-    my_tuple = read_csv('./movies.csv')
+    my_tuple = read_csv('./Movies.csv')
 
-    #print(Movies1(my_tuple))
+    # print(Movies1(my_tuple))
 
-    #print(Movies2(my_tuple))
+    # print(Movies2(my_tuple))
 
-    #my_fun = Movies3('Drama,Romance')
+    # print(Movies3(my_tuple))
+
+    # print(Movies4(my_tuple))
+
+    #my_fun = Movies5('Drama,Romance')
     #print(my_fun(my_tuple))
 
-    #my_fun = Movies4(my_tuple)
+    #my_fun = Movies6(my_tuple)
     #print(my_fun('Excelente'))
     #print(my_fun('Buena'))
     #print(my_fun('Mala'))
-
-    # print(Movies5(my_tuple))
-    # print(Movies6(my_tuple))
 
     # my_fun = Movies7("8.5","2000")
     # print(my_fun(my_tuple))
@@ -141,9 +145,9 @@ if __name__ == '__main__':
     # my_fun = Movies8(my_tuple)
     # print(my_fun())
 
-    #my_fun = Movies9()
-    #c = map(my_fun,my_tuple)
-    #print(list(c))
+    # my_fun = Movies9()
+    # c = map(my_fun,my_tuple)
+    # print(list(c))
 
-    #my_fun = Movies10(my_tuple)
-    #print(my_fun())
+    # my_fun = Movies10(my_tuple)
+    # print(my_fun())
